7223572f64d34cb3a88e9134fda85e596890ca8b
V2 <v2@catchdesign.co.nz>
2019-02-12 15:41:30 +1300

    deps

--- src/abc/code/Classes/DataObjectSearch.php
+++ src/abc/code/Classes/DataObjectSearch.php
@@ -23,6 +23,7 @@ class DataObjectSearch implements Flushable {
      */
     public static function flush() {
         //SS_Cache::factory('DataObjectSearch')->clean(Zend_Cache::CLEANING_MODE_ALL);
+        $cache = Injector::inst()->get(CacheInterface::class . '.DataObjectSearch');
         $cache->clear();
     }
 
