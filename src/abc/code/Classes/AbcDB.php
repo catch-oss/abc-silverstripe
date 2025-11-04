602c03c815a3d38ff45a6071120ade8123e6bb67
Az Ilari <aaron@catchdesign.co.nz>
2025-11-05 07:46:37 +1300

    args

--- src/abc/code/Classes/AbcDB.php
+++ src/abc/code/Classes/AbcDB.php
@@ -6,7 +6,7 @@ class AbcDB extends PDO{
 
 	protected static $instance = null;
 
-	public function __construct($dsn = null, $username = null, $password = null, array $driver_options = null){
+	public function __construct(?string $dsn = null, ?string $username = null, ?string $password = null, ?array $driver_options = null){
 
 		// Get the System DB Config
 		global $databaseConfig;
